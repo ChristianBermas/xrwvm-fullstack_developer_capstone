384b095b164477e1dfcafd7e6ee27c018742b37d
Christian Bermas <changianan588@outlook.com>
2026-06-21 14:19:36 +0800

    fix: models.py, populate.py, restapis.py, urls.py, views.py, settings.py

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -1,13 +1,10 @@
 # Uncomment the following imports before adding the Model code
 
 from django.db import models
-from django.utils.timezone import now
+# from django.utils.timezone import now
 from django.core.validators import MaxValueValidator, MinValueValidator
 
 
-# Create your models here.
-
-# <HINT> Create a Car Make model `class CarMake(models.Model)`:
 class CarMake(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField()
@@ -16,9 +13,8 @@ class CarMake(models.Model):
         return self.name
 
 
-# <HINT> Create a Car Model model `class CarModel(models.Model):`:
 class CarModel(models.Model):
-    car_make = models.ForeignKey(CarMake, on_delete=models.CASCADE) # Many-to-One relationship
+    car_make = models.ForeignKey(CarMake, on_delete=models.CASCADE)
     name = models.CharField(max_length=100)
     CAR_TYPES = [
         ('SEDAN', 'Sedan'),
